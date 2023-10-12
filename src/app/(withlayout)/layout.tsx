f7b4418af2809716994de2d1ab26d27c4433aed2
Arman-Kabir <armankabir8@gmail.com>
2023-10-13 00:47:46 +0600

    deshboard added

---
+++ src/app/(withlayout)/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Contents from "@/components/ui/Contents";
+import SideBar from "@/components/ui/Sidebar";
+import { Layout } from "antd";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Layout hasSider>
+      <SideBar />
+      <Contents>{children}</Contents>
+    </Layout>
+  );
+};
+
+export default DashboardLayout;
