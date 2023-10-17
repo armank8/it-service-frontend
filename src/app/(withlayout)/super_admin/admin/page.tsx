48148592d56f9a53a3cc23fbaf852019a8d66c28
Arman-Kabir <armankabir8@gmail.com>
2023-10-17 13:28:03 +0600

    re-usable table

--- src/app/(withlayout)/super_admin/admin/page.tsx
+++ src/app/(withlayout)/super_admin/admin/page.tsx
@@ -11,6 +11,51 @@ const ManageAdminPage = () => {
     const { role } = getUserInfo() as any;
     console.log(role);
 
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+            // sorter: true
+            sorter: (a: any, b: any) => a.age - b.age
+        },
+        {
+            title: 'Action',
+            render: function (data: any) {
+                return <Button onClick={() => console.log(data)} type="primary" danger>X</Button>
+            }
+
+        },
+    ];
+
+    const tableData = [
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+
+        },
+    ];
+
+    const onTableChange = (pagination: any, filter: any, sorter: any) => {
+        const { order, field } = sorter;
+        console.log(order, field);
+    };
+    const onPaginationChange = (page: number, pageSize: number) => {
+        console.log("Page:", page, "pageSize:", pageSize);
+    };
+
     return (
         <div>
             <UMBreadCrumb
@@ -33,8 +78,17 @@ const ManageAdminPage = () => {
                 </Link>
             </ActionBar>
 
-            <IHTable></IHTable>
-        </div>
+            <IHTable
+                loading={false}
+                columns={columns}
+                dataSource={tableData}
+                pageSize={5}
+                totalPages= {10}
+                showSizeChanger= {true}
+                onPaginationChange={onPaginationChange}
+                onTableChange={onTableChange}
+            ></IHTable>
+        </div >
     )
 }
 
--- src/components/ui/IHTable.tsx
+++ src/components/ui/IHTable.tsx
@@ -1,54 +1,39 @@
 "use client";
 import { Button, Table } from "antd"
 
-const IHTable = () => {
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
-            // sorter: true
-            sorter:(a:any,b:any)=>a.age - b.age
-        },
-        {
-            title: 'Action',
-            render:function(data:any){
-                return <Button onClick={()=>console.log(data)} type="primary" danger>X</Button>
-            }
-            
-        },
-    ];
-    const tableData = [
-        {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
+type IHTableProps = {
+    loading?: boolean
+    columns: any
+    dataSource: any
+    pageSize?: number
+    totalPages?: number
+    showSizeChanger?: boolean
+    onPaginationChange?: (page: number, pageSize: number) => void
+    onTableChange?: (pagination: any, filter: any, sorter: any) => void
+};
 
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
+const IHTable = ({
+    loading = false,
+    columns,
+    dataSource,
+    pageSize,
+    totalPages,
+    showSizeChanger = true,
+    onPaginationChange,
+    onTableChange,
+}: IHTableProps) => {
 
-        },
-    ];
-    const onPaginationChange = (page: number, pageSize: number) => {
-        console.log("Page:", page, "pageSize:", pageSize);
-    };
+
+
     const paginationConfig = {
-        pageSize: 5, total: 10, pageSizeOptions: [5, 10, 20],
-        showSizeChanger: true, onChange: onPaginationChange,
+        pageSize: 5,
+        total: 10,
+        pageSizeOptions: [5, 10, 20],
+        showSizeChanger: true,
+        onChange: onPaginationChange,
     };
 
-    const onTableChange = (pagination: any, filter: any, sorter: any) => {
-        const { order, field } = sorter;
-        console.log(order, field);
-    }
+
 
     return (
         <Table loading={false} columns={columns} dataSource={tableData}
