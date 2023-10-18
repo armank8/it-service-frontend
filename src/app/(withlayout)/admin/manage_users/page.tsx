c738f5bba7cbed0fa7e5d06a480fdf935da95fa2
Arman-Kabir <armankabir8@gmail.com>
2023-10-18 18:53:44 +0600

    Manage Users done

--- src/app/(withlayout)/admin/manage_users/page.tsx
+++ src/app/(withlayout)/admin/manage_users/page.tsx
@@ -1,13 +1,67 @@
 "use client";
 
+import Loading from "@/app/loading";
+import IHTable from "@/components/ui/IHTable";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { useGetUsersQuery } from "@/redux/api/userApi";
 import { getUserInfo } from "@/services/auth.service";
 import { Button } from "antd"
 import Link from "next/link"
 
 const ManageUsersPage = () => {
+
   const { role } = getUserInfo() as any;
-  console.log(role);
+  // console.log(role);
+  const {data,isLoading} = useGetUsersQuery(undefined);
+  console.log(data);
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
+  const columns = [
+    {
+        title: 'Id',
+        dataIndex: '_id',
+        key: '_id',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Role',
+        dataIndex: 'role',
+        key: 'role',
+    },
+    // {
+    //     title: 'Age',
+    //     dataIndex: 'age',
+    //     key: 'age',
+    //     // sorter: true
+    //     sorter: (a: any, b: any) => a.age - b.age
+    // },
+    {
+        title: 'Action',
+        render: function (data: any) {
+            return <Button onClick={() => console.log(data)} type="primary" danger>X</Button>
+        }
+
+    },
+];
+
+  const onPaginationChange = (page: number, pageSize: number) => {
+    console.log("Page:", page, "PageSize:", pageSize);
+    // setPage(page);
+    // setSize(pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    // setSortBy(field as string);
+    // setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
   return (
     <div>
       <UMBreadCrumb
@@ -22,11 +76,22 @@ const ManageUsersPage = () => {
           },
         ]}
       />
-      <h1>Manage Users Page</h1>
+      <h1>Manage Users Page</h1>          
+
       <h2>Users List</h2>
-      <Link href="/admin/manage_users/create">
-        <Button type="primary">Create</Button>
-      </Link>
+      <IHTable
+        loading={isLoading}
+        columns={columns}
+        dataSource={data}
+        // pageSize={size}
+        // totalPages={meta?.total}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      >
+      </IHTable>
+
     </div>
   )
 }
