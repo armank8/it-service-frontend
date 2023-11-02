9585cd29c1852a1aa7cf9cd400e577a378a9b2a0
Arman-Kabir <armankabir8@gmail.com>
2023-11-02 08:19:20 +0600

    sass added, globals rules added, hero-sectiopn done

--- src/components/homepage-ui/HeroSection.tsx
+++ src/components/homepage-ui/HeroSection.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
-import styles from './HeroSection.module.css';
+import styles from './HeroSection.module.scss';
 import serviceSvg from '../../assets/service.svg';
 const HeroSection = () => {
     return (
         <div className={styles.heroSection}>
             <div className={styles.leftSide}>
-                <h3>We provide you the</h3>
-                <h1>Best IT Service in the town</h1>
-                <p>24x7  service</p>
+                <div>
+                    <h3 style={{ textAlign: 'left' }}>We provide you the</h3>
+                    <h1>Best IT Service in the town</h1>
+                    <p>24x7  service</p>
+                </div>
             </div>
             <div className={styles.rightSide}>
-                <Image src={serviceSvg} alt='service svg' width={400} height={400}></Image>
+                {/* <Image src={serviceSvg} alt='service svg' width={400} height={400}></Image> */}
 
             </div>
         </div>
