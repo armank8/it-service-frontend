99ee1c0ee31821b60115b8b89abc4aac62b0b042
Arman-Kabir <armankabir8@gmail.com>
2024-05-08 03:38:30 +0600

    overview testing file and img file added

--- src/components/ui/Overview.tsx
+++ src/components/ui/Overview.tsx
@@ -2,23 +2,25 @@ import { Button, Col, Row, Statistic } from 'antd';
 
 const ServiceOverview = () => {
     return (
-        <section className='section_container'>
-            <h2 className='section_header'>Our  Service Overview</h2>
-            <Row gutter={16}>
-                <Col span={12}>
-                    <Statistic title="Happy Clients" value={112893} />
-                </Col>
-                <Col span={12}>
-                    <Statistic title="In Industry" value={`10+ years`} />
-                </Col>
-                <Col span={12}>
-                    <Statistic title="Yearly Revenue" value={112893} precision={2} />
+        <section className='overview'>
+            <div className="section_container">
+                <h2 className='section_header'>Our  Service Overview</h2>
+                <Row gutter={16}>
+                    <Col span={12}>
+                        <Statistic title="Happy Clients" value={112893} />
+                    </Col>
+                    <Col span={12}>
+                        <Statistic title="In Industry" value={`10+ years`} />
+                    </Col>
+                    <Col span={12}>
+                        <Statistic title="Yearly Revenue" value={112893} precision={2} />
 
-                </Col>
-                <Col span={12}>
-                    <Statistic title="Active Engineers" value={109} />
-                </Col>
-            </Row>
+                    </Col>
+                    <Col span={12}>
+                        <Statistic title="Active Engineers" value={109} />
+                    </Col>
+                </Row>
+            </div>
         </section>
     )
 }
